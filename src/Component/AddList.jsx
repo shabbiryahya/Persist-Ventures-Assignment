69b929abf80c1896f20ad0405cd553dc8048bbcb
Shabbir Yahya <shabbirkatlariwala@gmail.com>
2023-08-26 18:57:41 +0530

    Added:Assignment

--- src/Component/AddList.jsx
+++ src/Component/AddList.jsx
@@ -9,11 +9,10 @@ const [text,setText]=useState({
 const [list,setList]=useState([]);
 const [showSuggestion,setShowSuggestion]=useState(false);
 const[suggesttedData,setSuggestedData]=useState([]);
+const[indexed,setIndexed]=useState(0);
 
-useEffect(()=>{
 
- console.log(suggesttedData);
-},[showSuggestion])
+
 
 
 const onChange=(e)=>{
@@ -35,22 +34,32 @@ const addList=(e)=>{
 }
 
 const onchangeInput=(ind)=>{
-  
+  setIndexed(ind);
 const value=event.target.value;
+
 
   const updatedList=list.map((element,index)=>{
 
    return ind===index?{...element,note:value}:element;
 
   })
-console.log(updatedList);
+// console.log(updatedList);
   setList([...updatedList]);
   if(updatedList[ind].note.includes("<>"))
   {
+    
     setShowSuggestion(true);
     // console.log("Yes");
-    // const data=
+    const newList = list.slice(ind + 1).map((element) => {
+      return element;
+    });
+
+    // console.log(newList);
+    setSuggestedData([...newList]);
+
     
+       
+      
     
   }
   else
@@ -59,6 +68,27 @@ console.log(updatedList);
     // console.log("No");
   }
   
+}
+
+const changeListData=(ind,value)=>{
+
+  //  console.log(indexed);
+   let addedData="";
+
+   suggesttedData.slice(ind+1).map((element) => {
+   addedData+="<>"+element.note;
+  });
+  console.log(addedData);
+   const updatedList=list.map((element,index)=>{
+  //  console.log(ind,index);
+    const char=element.note;
+
+    return indexed===index?{...element,note:char+value+addedData}:element;
+ 
+   })
+
+setList([...updatedList]);
+setShowSuggestion(false);
 }
   return (
     <>
@@ -69,6 +99,7 @@ console.log(updatedList);
       name="note"  
       placeholder='Enter a List...' 
       onChange={onChange}
+      
       value={text.note}
      />
     <div className='flex bg-emerald-200 rounded-full py-2 pl-3 pr-3  justify-center items-center cursor-pointer' 
@@ -79,14 +110,18 @@ console.log(updatedList);
    </div>
  <div >
  {showSuggestion && (
-          <ul className='m-auto'>
+          <ul className= 'fixed right-40 top-28 bg-slate-300 w-24'>
               {suggesttedData.map((suggestion, index) => (
                
                <li
                   key={index}
-                  className="text-blue-900 cursor-pointer transition-colors duration-300 hover:text-blue-600"
+                  className="cursor-pointer transition-colors duration-300 hover:text-blue-600 "
+                  onClick={()=>{
+                     
+                    changeListData(index,suggestion.note);
+                  }}
                 >
-                  {suggestion}
+                  {suggestion.note}
                 </li>
               ))}
             </ul>
@@ -95,20 +130,31 @@ console.log(updatedList);
    
 
    <div className="m-20 mx-auto max-w-screen-md ">
-  <div className={`block max-w-md mx-auto p-10 rounded-md ${list.length>0&&`shadow-md`} overflow-y-auto max-h-60 custom-scrollbar`}>
+  <div className={` block max-w-md mx-auto p-10 rounded-md ${list.length>0&&`shadow-md`} overflow-y-auto max-h-60 custom-scrollbar`}>
    
     {
 
       list.map((element,index)=>{
 
       return (
-      <div key={index+1}>
-       <input  type="text" className="peer h-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-pink-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" value={element.note} onChange={()=>{
+      <div key={index+1} className='' >
+
+       <input type="text"
+        className={`font-bold peer h-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-pink-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 ${
+          element.note.includes('<>') ? 'blue-brackets' : ''
+        }`}
+        data-text={element.note.substring(element.note.indexOf('<>') + 2)} 
+         value={element.note}
+          onChange={()=>{
 
         onchangeInput(index);
+        
+        
        }}/> 
        
+       
        <br />
+       
 
       </div>
       
